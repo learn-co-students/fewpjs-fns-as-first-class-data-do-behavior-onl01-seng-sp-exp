5fec8df43d24d97c6320719ac0c07d5357190862
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-10-16 12:52:08 -0700

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,17 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(arg) {
+  const time = parseInt(arg, 10);
+  if (time < 12) {
+    return "Good Morning";
+  } else if (time > 17) {
+    return "Good Evening";
+  } else {
+    return  "Good Afternoon";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message){
+  document.querySelector('#greeting').innerText = message
+}
